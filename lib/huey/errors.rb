cd49ebb01256291257c29015eed8b9d1d3aaccc6
David Briggs <godamonra@gmail.com>
2013-03-14 14:52:21 -0400

    add additional errors

--- lib/huey/errors.rb
+++ lib/huey/errors.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 module Huey
-
   # All the error specific to Huey.
   module Errors
 
@@ -13,6 +12,18 @@ module Huey
 
     # PressLinkButton is raised if the link button hasn't been pressed yet.
     class PressLinkButton < Error; end
+
+    # The bulb can't be updated since it is off
+    class BulbOff < Error; end
+
+    # Cannot add bulb to group since it is full
+    class GroupTableFull < Error; end
+
+    # Cannot make request due to missing parameters
+    class MissingParameters < Error; end
+
+    # Error occured in the bridge, not the request
+    class InternalBridgeError < Error; end
 
     # HueResponseError is raised if we receive an odd response from the Hue.
     class HueResponseError < Error; end
